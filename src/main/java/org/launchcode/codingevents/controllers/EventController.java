98f6527bf661433eb308aca13cfdd9ad1315f258
S W <sw@Ss-MacBook-Air.local>
2022-10-16 15:41:46 -0500

    Created Event Data layer

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,12 +16,11 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    //this makes the arraylist of events accessible throughout class
-    private static List<Event> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+//        model.addAttribute("title", "All Events");
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -33,7 +33,7 @@ public class EventController {
     //redirect: to root path for this specific controller which is /events
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
-        events.add(new Event(eventName, eventDescription));
+        EventData.add(new Event(eventName, eventDescription));
         return "redirect:";
     }
 
